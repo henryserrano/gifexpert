d859bf71c88468f7eaf39af8bb1e8901061ddb67
Henry Serrano <henryserrano@hotmail.com>
2022-01-30 10:36:27 +1100

    grid

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,37 +1,45 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const GifGrid = ({ category }) => {
 
-    const [counter, setcounter] = useState(0);
+    const [images, setImages] = useState([]);
 
-    useEffect( () => {
-      getGifts();
-  
+
+
+    useEffect(() => {
+        getGifts();
+
     }, [])
 
     const getGifts = async () => {
         const url = "https://api.giphy.com/v1/gifs/search?q=los simpsons&limit=10&api_key=TP5ydAJBGPrCaCEBUhEdUOKkmwlFx1Hp";
         const resp = await fetch(url);
-        const {data}= await resp.json();
+        const { data } = await resp.json();
 
-        const gifs=data.map( img =>{
+        const gifs = data.map(img => {
             return {
-                id:img.id,
+                id: img.id,
                 title: img.title,
-                url:img.images?.downsized_medium.url
+                url: img.images?.downsized_medium.url
             }
         })
 
         console.log(gifs);
+        setImages(gifs);
     }
 
-   // getGifts();
+    // getGifts();
 
 
 
     return <div>
         <h3>{category}</h3>
-        <h3>{counter}</h3>
-        <button onClick={()=>setcounter(counter+1)}></button>
+        <ol>
+            {images.map(img => (
+                <li key={img.id} >{img.title}</li>
+            ))
+            }
+        </ol>
+
     </div>;
 };
